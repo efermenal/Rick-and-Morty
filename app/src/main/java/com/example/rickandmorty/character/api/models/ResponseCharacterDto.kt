67201ea8690c842151acfdc76b7f7e04cef7d3f3
Endherson <ferendher@gmail.com>
2022-05-06 00:21:35 -0400

    Refactor ResponseCharacterDto add Moshi's custom fields name

--- app/src/main/java/com/example/rickandmorty/character/api/models/ResponseCharacterDto.kt
+++ app/src/main/java/com/example/rickandmorty/character/api/models/ResponseCharacterDto.kt
@@ -1,6 +1,8 @@
 package com.example.rickandmorty.character.api.models
 
+import com.squareup.moshi.Json
+
 data class ResponseCharacterDto(
-    val info: InfoDto,
-    val characters: List<CharacterDto>
+    @Json(name = "info") val info: InfoDto,
+    @Json(name = "results") val characters: List<CharacterDto>
 )
