ccd635f4acd134c7e8889603d402734fe7f8554e
Endherson <ferendher@gmail.com>
2022-05-06 16:00:04 -0400

    refactor(fragment and views): add views for fragments

--- app/src/main/java/com/example/rickandmorty/character/ui/CharacterFragment.kt
+++ app/src/main/java/com/example/rickandmorty/character/ui/CharacterFragment.kt
@@ -1,8 +1,35 @@
 package com.example.rickandmorty.character.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rickandmorty.R
+import com.example.rickandmorty.databinding.FragmentCharacterBinding
 
 class CharacterFragment : Fragment(R.layout.fragment_character) {
+
+    private var _binding: FragmentCharacterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCharacterBinding.inflate(inflater, container, false)
+        configureUI()
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun configureUI() {
+        // TODO: set views
+    }
 
 }
