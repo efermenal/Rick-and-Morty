074e5df39e8dff12f82634598036c7674d2f3571
Endherson <ferendher@gmail.com>
2022-05-06 01:16:00 -0400

    feat(gradle): add navigation component
    set up navigation component and include CharacterFragment

---
+++ app/src/main/java/com/example/rickandmorty/character/ui/CharacterFragment.kt
@@ -0,0 +1,8 @@
+package com.example.rickandmorty.character.ui
+
+import androidx.fragment.app.Fragment
+import com.example.rickandmorty.R
+
+class CharacterFragment : Fragment(R.layout.fragment_character) {
+
+}
