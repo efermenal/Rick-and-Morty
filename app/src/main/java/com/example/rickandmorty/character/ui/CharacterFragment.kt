12e9f3de56c3af6e7f239b748472275dbdaa4bde
Endherson <ferendher@gmail.com>
2022-05-06 17:07:41 -0400

    fix(character fragment): add main functionality

--- app/src/main/java/com/example/rickandmorty/character/ui/CharacterFragment.kt
+++ app/src/main/java/com/example/rickandmorty/character/ui/CharacterFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.rickandmorty.R
+import com.example.rickandmorty.character.ui.adapter.CharacterAdapter
 import com.example.rickandmorty.databinding.FragmentCharacterBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class CharacterFragment : Fragment(R.layout.fragment_character) {
+@AndroidEntryPoint
+class CharacterFragment : Fragment(R.layout.fragment_character), CharacterAdapter.Listener {
 
+    private val viewModel: CharacterViewModel by viewModels()
     private var _binding: FragmentCharacterBinding? = null
     private val binding get() = _binding!!
+    private lateinit var characterAdapter: CharacterAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +31,8 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
     ): View? {
         _binding = FragmentCharacterBinding.inflate(inflater, container, false)
         configureUI()
+        observeViewModel()
+        viewModel.getCharacters()
         return binding.root
     }
 
@@ -28,8 +41,31 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
         _binding = null
     }
 
+    override fun onClickCharacter() {
+        TODO("Not yet implemented")
+    }
+
     private fun configureUI() {
-        // TODO: set views
+        characterAdapter = CharacterAdapter(this)
+        binding.characterContentView.apply {
+            adapter = characterAdapter
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.viewState
+            .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .onEach {
+                renderUI(it)
+            }.launchIn(lifecycleScope)
+    }
+
+    private fun renderUI(characterState: CharacterViewModel.ViewState) {
+        when (characterState.isLoading) {
+            true -> {}
+            false -> {}
+        }
+        characterAdapter.submitList(characterState.characters)
     }
 
 }
