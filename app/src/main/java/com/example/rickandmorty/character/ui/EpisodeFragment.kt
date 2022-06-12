6336f32953fb7e79935c477c82598f7f9edf54da
Endherson <ferendher@gmail.com>
2022-06-12 14:59:49 -0400

    refactor(EpisodeFragment): add basic functionality

--- app/src/main/java/com/example/rickandmorty/character/ui/EpisodeFragment.kt
+++ app/src/main/java/com/example/rickandmorty/character/ui/EpisodeFragment.kt
@@ -5,22 +5,28 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.rickandmorty.R
+import com.example.rickandmorty.character.ui.adapter.EpisodeAdapter
 import com.example.rickandmorty.databinding.FragmentEpisodeBinding
 
 
 class EpisodeFragment : Fragment(R.layout.fragment_episode) {
 
     private val args: EpisodeFragmentArgs by navArgs()
+    private lateinit var episodeAdapter: EpisodeAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentEpisodeBinding.bind(view)
 
+        setAdapter(binding)
+    }
+
+    private fun setAdapter(binding: FragmentEpisodeBinding) {
+        episodeAdapter = EpisodeAdapter()
         with(binding) {
-            // episodeContentView.adapter =
+            episodeContentView.adapter = episodeAdapter
         }
-
-
+        episodeAdapter.submitList(args.episodeList.episodes)
     }
 
 }
